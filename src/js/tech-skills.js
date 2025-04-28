8259e0a82f2995ce0bd67bbd4e4e37dca86396b3
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-04-28 17:51:57 +0300

    markup-js-marquee- paused

--- src/js/tech-skills.js
+++ src/js/tech-skills.js
@@ -1,11 +1,44 @@
+const SKILLS = [
+    'HTML/CSS',
+    'JavaScript',
+    'React',
+    'Node. js',
+    'React Native',
+    'Typescript'
+]
+
 const MARQUEE_INNER = document.querySelector(".marquee-inner");
 const MARQUEE_INNER_ROTATE = document.querySelector(".marquee-inner-rotate");
 
+function createMarkup(array) {
+    return array.map((item) => `
+    <span class="marquee-line">${item}</span>
+    <span class="marquee-dot"></span>
+    `).join("");
+}
+
+function createMarkupRotate(array) {
+    return array.map((item) => `
+    <span class="marquee-line-rotate">${item}</span>
+    <span class="marquee-dot-rotate"></span>
+    `).join("");
+}
+
+MARQUEE_INNER.insertAdjacentHTML("beforeend", createMarkup(SKILLS));
+MARQUEE_INNER_ROTATE.insertAdjacentHTML("beforeend", createMarkupRotate(SKILLS));
+
 function doubleContent(element) {
     if (!element) return;
-    const CONTENT = element.innerHTML;
-    element.innerHTML += CONTENT;
+    const content = element.innerHTML;
+    element.innerHTML +=content;
 }
 
 doubleContent(MARQUEE_INNER);
 doubleContent(MARQUEE_INNER_ROTATE);
+
+function toggleAnimation() {
+    this.classList.toggle("paused")
+}
+
+MARQUEE_INNER.addEventListener("click", toggleAnimation);
+MARQUEE_INNER_ROTATE.addEventListener("click", toggleAnimation);
