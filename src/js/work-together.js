27d11d43a3c6dd338858e560be86f91bb6079e48
Yuri Boblak <sseslesnes@gmail.com>
2025-05-01 11:28:33 +0300

    temp221

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,28 +1,19 @@
 const form = document.querySelector('.form');
-const emailInput = form.querySelector('input[type="text"]');
-const messageInput = form.querySelector('textarea');
+// const emailInput = form.querySelector('input[type="text"]');
+// const messageInput = form.querySelector('textarea');
 const emailErrorMsg = document.querySelector('.form-error');
 const emailValidMsg = document.querySelector('.email-valid');
 const popupOverlay = document.getElementById('popupOverlay');
+const popupCloseBtn = document.querySelector('.popup-close');
 const popup = document.getElementById('popup');
-const closeBtn = document.querySelector('.popup-close');
+
 let isValid = true;
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  validateForm();
-});
-
-form.addEventListener('input', function (event) {
-  emailErrorMsg.style.display = 'none';
-  emailValidMsg.style.display = 'none';
-  if (isValidEmail(emailInput.value.trim())) {
-    emailValidMsg.style.display = 'block';
-  }
-});
-
 function validateForm() {
-  if (!isValidEmail(emailInput.value.trim())) {
+  const formData = new FormData(form);
+  const email = formData.get('email').trim();
+
+  if (!isValidEmail(email)) {
     emailErrorMsg.style.display = 'block';
     isValid = false;
   } else {
@@ -42,10 +33,25 @@ function openModal() {
   popup.classList.remove('visually-hidden');
 }
 
-const closeModalButton = document.querySelector('.popup-close');
-if (closeModalButton) {
-  closeModalButton.addEventListener('click', () => {
-    popupOverlay.classList.add('visually-hidden');
-    popup.classList.add('visually-hidden');
-  });
+function popupClose() {
+  popupOverlay.classList.add('visually-hidden');
+  popup.classList.add('visually-hidden');
 }
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  validateForm();
+});
+
+form.addEventListener('input', function (event) {
+  const formData = new FormData(form);
+  const email = formData.get('email').trim();
+
+  emailErrorMsg.style.display = 'none';
+  emailValidMsg.style.display = 'none';
+  if (isValidEmail(email)) {
+    emailValidMsg.style.display = 'block';
+  }
+});
+
+popupCloseBtn.addEventListener('click', popupClose);
