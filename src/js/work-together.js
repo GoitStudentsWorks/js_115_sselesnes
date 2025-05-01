4018c38a7b7fca1f3f6f2579ee4d89fce27cde25
Yuri Boblak <sseslesnes@gmail.com>
2025-05-01 12:33:04 +0300

    temp223

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -8,6 +8,7 @@ const popupCloseBtn = document.querySelector('.popup-close');
 const popup = document.getElementById('popup');
 
 let isValid = true;
+let scrollbarWidth = 0;
 
 function validateForm() {
   const formData = new FormData(form);
@@ -17,7 +18,6 @@ function validateForm() {
     emailErrorMsg.style.display = 'block';
     isValid = false;
   } else {
-    form.reset();
     openModal();
     emailValidMsg.style.display = 'none';
   }
@@ -28,22 +28,61 @@ function isValidEmail(email) {
   return emailRegex.test(email);
 }
 
+// turn off the vertical scrollbar so body doesn't moving by offset
 function openModal() {
+  if (!scrollbarWidth) {
+    scrollbarWidth = getScrollbarWidth();
+  }
+
   popupOverlay.classList.remove('visually-hidden');
   popup.classList.remove('visually-hidden');
+
+  document.body.style.paddingRight = `${scrollbarWidth}px`;
+  document.body.style.transition = 'none';
+  document.body.style.overflow = 'hidden';
+
+  popupOverlay.addEventListener('click', handleOverlayClick);
+}
+
+function handleOverlayClick() {
+  popupClose();
 }
 
 function popupClose() {
+  form.reset();
   popupOverlay.classList.add('visually-hidden');
   popup.classList.add('visually-hidden');
+
+  document.body.style.paddingRight = '';
+  document.body.style.overflow = '';
+  popupOverlay.removeEventListener('click', handleOverlayClick);
+  // document.body.style.transition = 'all 0.3s ease';git
 }
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  validateForm();
-});
+function getScrollbarWidth() {
+  const outer = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.overflow = 'scroll';
+  outer.style.width = '100px';
+  outer.style.height = '100px';
+  document.body.appendChild(outer);
 
-form.addEventListener('input', function (event) {
+  const inner = document.createElement('div');
+  inner.style.width = '100%';
+  inner.style.height = '100%';
+  outer.appendChild(inner);
+
+  const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+  document.body.removeChild(outer);
+  return scrollbarWidth;
+}
+
+function formSubmitHandle(event) {
+  event.preventDefault();
+  validateForm();
+}
+
+function formInputHandle(event) {
   const formData = new FormData(form);
   const email = formData.get('email').trim();
 
@@ -52,6 +91,8 @@ form.addEventListener('input', function (event) {
   if (isValidEmail(email)) {
     emailValidMsg.style.display = 'block';
   }
-});
+}
 
+form.addEventListener('submit', formSubmitHandle);
+form.addEventListener('input', formInputHandle);
 popupCloseBtn.addEventListener('click', popupClose);
